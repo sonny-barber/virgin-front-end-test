25fd14dabcd02b22ea126916e8810f78b9a792aa
Sonny Barber <sonnybarber04@gmail.com>
2024-01-28 19:24:19 +0000

    package updates and configure sass

--- next.config.js
+++ next.config.js
@@ -1,31 +1,45 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true' && isProd,
-})
+  enabled: process.env.ANALYZE === 'true' && isProd,
+});
+
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	reactStrictMode: false,
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	async headers() {
-		return [
-		  {
-			source: '/:all*(svg|jpg|png|avif|webp|ttf|woff|woff2|otf)',
-			locale: false,
-			headers: [
-			  {
-				key: 'Cache-Control',
-				value: 'public, max-age=9999999999, must-revalidate',
-			  }
-			],
-		  },
-		]
-	},
-}
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  swcMinify: true,
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|avif|webp|ttf|woff|woff2|otf)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd3hk78fplavsbl.cloudfront.net',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
